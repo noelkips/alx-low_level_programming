00ab81c4fa9e96606f530ad6781b6e9545d2216f
Noel .k. Langat <noelkipronolangat@gmail.com>
2022-12-08 19:20:31 +0000

    Add task 1-6 c hello world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always return 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper gramma, but the outcome is a piece of art,\n");
+	return (0);
+}
