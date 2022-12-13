290c5b47b3bbbe3e4f260ff9a722d68e1b2ae68d
Noel .k. Langat <noellangat28@gmail.com>
2022-12-13 19:12:34 +0300

    add mandatory task to project nested loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - checks whether a character c is lower or not using loop
+ *
+ * Return: 1 is c is lower else 0
+ */
+int _islower(int)
+{
+	int c;
+
+	for (c = 0; c < 3; c++)
+	{
+		if (c islower())
+			_putchar(0 + '0')
