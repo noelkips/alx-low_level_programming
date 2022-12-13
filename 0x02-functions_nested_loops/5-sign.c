feb65a1fd6ab5c9910417f49aead2fa94e2e6960
Noel .k. Langat <noellangat28@gmail.com>
2022-12-13 18:24:46 +0000

    Add mandatory tasks to project nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,7 +13,8 @@ int print_sign(int n)
 	{
 		_putchar('+');
 		return (1);
-	} else if (n == 0)
+	}
+	if (n == 0)
 	{
 		_putchar('0');
 		return (0);
@@ -23,4 +24,5 @@ int print_sign(int n)
 		_putchar('-');
 		return (-1);
 	}
+	return (0);
 }
