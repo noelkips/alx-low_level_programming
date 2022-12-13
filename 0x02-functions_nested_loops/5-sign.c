551113299ec78011d983757497cb5c39f5fb914f
Noel .k. Langat <noellangat28@gmail.com>
2022-12-13 18:13:54 +0000

    Add mandatory tasks to project nested loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - check whether a number is signed or not
+ *
+ *@n: input number
+ *
+ * Return: 1 if signed (positve) else 0
+ */
+int print_sign(int n)
+{
+	if (n >= 1)
+	{
+		_putchar('+');
+		return (1);
+	} else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	} else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs -returns absolute value of an integer
+ *
+ *@n: number input
+ *
+ * Return: n, absolute vale for an integer
+ */
+int _abs(int n)
+{
+	
+	if (n >= 0)
+		return (n);
+	else
+		return (n * -1);
+}
