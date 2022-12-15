2cd3c533b71707747d4d0cce18d8dba439edc2be
Noel .k. Langat <noellangat28@gmail.com>
2022-12-15 02:17:24 +0000

    Add advance tasks to project nested loops

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,23 +7,25 @@
  */
 int main(void)
 {
-	unsigned long a, b , c, temp_1, temp_2;
+	unsigned long a, b, c;
 	int i;
+
 	a = 0;
 	b = 1;
-
 	for (i = 1; i <= 50 ; i++)
 	{
 		c = a + b;	
 		printf("%lu", c);
-		temp_1 = b;
-		temp_2 = c;
-		a = temp_1;
-		b = temp_2;
+		a = b;
+		b = c;
 		if (i == 50)
+		{
 			printf("\n");
+		}
 		else
+		{
 			printf(",");
+		}
 	}
 	return (0);
 }
