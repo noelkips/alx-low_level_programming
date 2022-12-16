26f35f1e9d382474b1b57c29a7b0301fa9e6f3ea
Noel .k. Langat <noellangat28@gmail.com>
2022-12-16 11:23:34 +0000

    Correct advance task 12 in 0x04 nested loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -17,7 +17,7 @@ void print_number(int n)
 	{
 		_putchar('0');
 	}
-	else if (n > 0 && n < 10 )
+	else if (n > 0 && n < 10)
 	{
 		_putchar(n % 10 + '0');
 	}
@@ -39,6 +39,7 @@ void print_number(int n)
 		_putchar((n / 10) % 100 + '0');
 		_putchar(n % 10 + '0');
 	}
+	_putchar(' ');
 }
 
 
