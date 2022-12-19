c258bf82b2d8cd198f2b2ee55eba837120076f5f
Noel .k. Langat <noellangat28@gmail.com>
2022-12-19 19:25:03 +0000

    Add task-3 to Pointers_arrays_strings project

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _puts - prints a string on stdout
+ * @str: the string to print
+ * Return: none
+ */
+
+void _puts(char *str)
+{
+	int len = strlen(str);
+	int i;
+
+	for (i = 0 ; i <= len ; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
