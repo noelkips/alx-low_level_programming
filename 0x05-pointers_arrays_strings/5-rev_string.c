967001daf949a075ca87d5aeb82a5d1e96e5e038
Noel .k. Langat <noellangat28@gmail.com>
2022-12-19 20:11:38 +0000

    Add task-5 to Pointers_arrays_strings project

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,20 +1,20 @@
 #include "main.h"
 #include <string.h>
+#include <stdio.h>
 
 /**
- * print_rev - prints a string in reverse
+ * rev_string - prints a string in reverse
  * @s: the string to print
- * Return: none
+ * Return: string in reverse
  */
 
-void print_rev(char *s)
+void rev_string(char *s)
 {
 	int len = strlen(s);
 	int i;
 
 	for (i = len - 1 ; i >= 0 ; i--)
 	{
-		_putchar(s[i]);
+		printf("%c", s[i]);
 	}
-	_putchar('\n');
 }
