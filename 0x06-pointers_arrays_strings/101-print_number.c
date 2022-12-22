bcd9eff521f8d8c293ac190199f116266f37a7b0
Noel Langat <noellangat28@gmail.com>
2022-12-22 19:22:11 +0300

    Create 101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * print_number -prints integer
+ * @n: input integer to print
+ * Return: none
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		n = n * -1;
+		_putchar('-');
+	}
+	if (n == 0)
+	{
+		_putchar(n + '0');
+	}
+	else if (n > 0 && n < 10)
+	{
+		_putchar(n % 10 + '0');
+	}
+	else if (n >= 10 && n <= 99)
+	{
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+	else if (n >= 100 && n <= 999)
+	{
+		_putchar(n / 100 + '0');
+		_putchar((n / 10) % 10  + '0');
+		_putchar(n % 10 + '0');
+	}
+	else
+	{
+		_putchar(n / 1000 + '0');
+		_putchar((n / 100) % 10 + '0');
+		_putchar((n / 10) % 100 + '0');
+		_putchar(n % 10 + '0');
+	}
+}
