08d74207d6b16c4a9c34ce3db3e824e11eb87947
Noel Langat <noellangat28@gmail.com>
2022-12-22 19:15:21 +0300

    Update 100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - function that encodes a string using rot13.
+ * @s: string to encode
+ *
+ * Return: a pointer to sear
+ */
+char *rot13(char *str)
+{
+	int i;
+
+	for ( i = 0; str[i] != '\0', i++)
+	{
+		for (i = 0; ((str[i] >= 'a' && str[i] <= 'm') || (str[i] >= 'A' && str[i] <= 'M')); i++)
+		{
+			str[i] += 13;
+		}
+		if ((str[i] >=  'n' && str[i] <= 'z') || (str[i] >=  'N' && str[i] <= 'Z'))
+			str[i] -= 13;
+	}
+	return (str);
+}
