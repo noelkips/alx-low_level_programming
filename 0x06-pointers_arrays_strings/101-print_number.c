2c02a9fdbdf83d6d978d749fe14f9bf2e51ba329
Noel Langat <noellangat28@gmail.com>
2022-12-22 22:15:53 +0300

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,35 +8,26 @@
 
 void print_number(int n)
 {
+	unsigned int num;
+
+	if (n < 10 && n >= 0)
+	{
+		_putchar('0' + n);
+		return;
+	}
 	if (n < 0)
 	{
-		n = n * -1;
+		num = -n;
 		_putchar('-');
-	}
-	if (n == 0)
-	{
-		_putchar(n + '0');
-	}
-	else if (n > 0 && n < 10)
-	{
-		_putchar(n % 10 + '0');
-	}
-	else if (n >= 10 && n <= 99)
-	{
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
-	}
-	else if (n >= 100 && n <= 999)
-	{
-		_putchar(n / 100 + '0');
-		_putchar((n / 10) % 10  + '0');
-		_putchar(n % 10 + '0');
 	}
 	else
 	{
-		_putchar(n / 1000 + '0');
-		_putchar((n / 100) % 10 + '0');
-		_putchar((n / 10) % 100 + '0');
-		_putchar(n % 10 + '0');
+		num = n;
 	}
+
+	if (num >= 10)
+	{
+		print_number(num / 10);
+	}
+	_putchar('0' + (num % 10));
 }
