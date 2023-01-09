455895accf9ad64a1f36f93c2565d1d25f3def3e
Noel .k. Langat <noellangat28@gmail.com>
2023-01-09 19:12:54 +0000

    correct tasks

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- *main -Entry point, print string _putchar
- *
- * Return: Always  0 (Success)
- */
+ *  *main -Entry point, print string _putchar
+ *   *
+ *    * Return: Always  0 (Success)
+ *     */
 int main(void)
 {
 	char *ch = "_putchar";
-
+	
 	while (*ch)
 	{
 		_putchar(*ch);
