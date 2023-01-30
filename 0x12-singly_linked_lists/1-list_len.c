54de0efad528286e6c2f4dfcd07950ef94b72b77
Noel k Langat <noellangat28@gmail.com>
2023-01-30 16:55:17 +0000

    Add task 2 to linked list project

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,8 +1,7 @@
 #include "lists.h"
 
 /**
- * list_len - Finds the number of elements in
- *            a linked list_t list.
+ * list_len- returns the number of elements in a linked list_t
  * @h: The linked list_t list.
  *
  * Return: The number of elements in h.
