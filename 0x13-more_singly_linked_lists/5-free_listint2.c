922ad23d0a391d12022de155c676622a3ecbb1cd
Noel k Langat <noellangat28@gmail.com>
2023-02-02 20:03:20 +0000

    Update task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,13 +13,10 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-
-	while (*head)
+	if (head == NULL)
+		return;
+	while (*head != NULL)
 	{
-		if (head == NULL)
-		{
-			break;
-		}
 		temp = (*head)->next;
 		free(*head);
 		*head = temp;
