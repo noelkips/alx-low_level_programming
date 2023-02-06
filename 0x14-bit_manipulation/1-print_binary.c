f808850deb98da52d88804148de490af773da358
Noel k Langat <noellangat28@gmail.com>
2023-02-06 20:21:24 +0000

    Update 1, add 6

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -3,28 +3,25 @@
 /**
  * print_binary - prints the binary representation of a number
  * @n: parameter
- *
- * Return: none
  */
-
 void print_binary(unsigned long int n)
 {
-	int temp;
-	int count = 0;
+	int i, count = 0;
 	unsigned long int current;
 
-	for (temp = n; (temp >>= 1) > 0; count++)
-		;
-	for (; count >= 0; count--)
+	for (i = 63; i >= 0; i--)
 	{
-		current = n >> count;
+		current = n >> i;
 
 		if (current & 1)
 		{
 			_putchar('1');
+			count++;
 		}
-		else
+		else if (count)
 			_putchar('0');
 	}
+	if (!count)
+		_putchar('0');
 }
 
